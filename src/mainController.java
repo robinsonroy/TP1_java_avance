bd42219e9f2fe0e646cbf28d21aea4356950ac33
robinson Roy <robinson.roy@me.com>
2015-01-12 14:42:18 +0100

    git config

---
+++ src/mainController.java
@@ -0,0 +1,35 @@
+/**
+ * Created by Robinson on 12/01/15.
+ */
+
+import java.io.InputStream;
+        import java.lang.Process;
+        import java.io.IOException;
+
+/**
+ * Created by Robinson on 05/01/15.
+ */
+public class mainController {
+
+    public static void main(String[] args) {
+
+        Runtime r = Runtime.getRuntime();
+        Process p = null;
+        try {
+            p = r.exec("traceroute fr.wikipedia.org");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        InputStream myStream = p.getInputStream();
+
+        int i;
+        try {
+            while ((i = myStream.read()) != -1) {
+                System.out.print((char) i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
