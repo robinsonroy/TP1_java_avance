04c65ff084b60e4cb52e208b3da4fdb4c29291d0
robinson Roy <robinson.roy@me.com>
2015-01-12 15:12:39 +0100

    use fakeroute.jar mode

--- src/mainController.java
+++ src/mainController.java
@@ -16,7 +16,7 @@ public class mainController {
         Runtime r = Runtime.getRuntime();
         Process p = null;
         try {
-            p = r.exec("java -jar /Users/Robinson/Downloads/fakeroute.jar ece.fr");
+            p = r.exec("java -jar fakeroute.jar ece.fr");
         } catch (IOException e) {
             e.printStackTrace();
         }
